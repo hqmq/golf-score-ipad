f6ab08867ae9255788e8e08a2e3851f2b9a6bf9c
Mike Ries <michael@riesd.com>
2013-04-03 20:39:13 -0600

    adding more UI

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -1,9 +1,25 @@
 class RoundController < UIViewController
-  attr_reader :add_button
+  attr_reader :score_grid, :course_name
   def viewDidLoad
     view.styleId = 'round'
     view.when_tapped do
       presentingViewController.dismissViewControllerAnimated(true, completion:nil)
     end
+    init_score_grid
+    init_course_name
+  end
+
+  def init_score_grid
+    @score_grid = ScoreGrid.new
+    @score_grid.sizeToFit
+    view.addSubview(@score_grid)
+  end
+
+  def init_course_name
+    @course_name = UITextField.alloc.initWithFrame(CGRectZero)
+    course_name.styleClass = 'course-name'
+    course_name.setBackgroundColor(UIColor.clearColor)
+    course_name.setPlaceholder("Course Name")
+    view.addSubview(course_name)
   end
 end
---
+++ app/views/score_grid.rb
@@ -0,0 +1,7 @@
+class ScoreGrid < UIView
+  def init
+    ret = super
+    self.styleClass = "score-grid"
+    ret
+  end
+end
